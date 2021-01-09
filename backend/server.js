b7abde4961624f56b34e6931d215cc87a06169f9
Hüdai ERBAŞ <47556170+hudaierbas@users.noreply.github.com>
2021-01-09 15:09:36 +0300

    Add files via upload
    reupload

---
+++ backend/server.js
@@ -0,0 +1,91 @@
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
+
+// app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: "1134862",
+  key: "3af258d08bac1229e6db",
+  secret: "da40ef821f0fb704eda1",
+  cluster: "eu",
+  useTLS: true,
+});
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+/*
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+*/
+
+// db config
+const url =
+  "mongodb+srv://admin:pmkKqNx81cB8xeT6@cluster0.yo6tr.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A change occured", change);
+
+    const messageDetails = change.fullDocument;
+    if (change.operationType === "insert") {
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
+});
+
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/api/v1/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/api/v1/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
