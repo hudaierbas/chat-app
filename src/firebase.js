93d74087df5b5ba7a458aa8921c82cc80bd7ce40
Admin <hudaierbas@gmail.com>
2021-01-19 15:31:34 +0300

    eslintrc

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import firebase from "firebase/app";
 import "firebase/auth";
 
@@ -13,19 +12,3 @@ const app = firebase.initializeApp({
 
 export const auth = app.auth();
 export default app;
-=======
-import firebase from "firebase/app";
-import "firebase/auth";
-
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-});
-
-export const auth = app.auth();
-export default app;
->>>>>>> 3bce3e3203184b14dec8c97e5563c472cac2d810
